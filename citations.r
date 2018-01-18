651f6e52a29b29c2816846b5deadef4ba9bb36d5
Jordi Asher <jashera@essex.ac.uk>
2018-01-18 18:46:55 +0000

    Update citations.r

--- citations.r
+++ citations.r
@@ -14,7 +14,7 @@
 # Gets citations from Google Scholar
 library(scholar)
 # Google Scholar ID
-user = "VII2oEQAAAAJ"
+user = "lWsYRH8AAAAJ&hl"
 cit = get_citation_history(user)
 cit$year = paste("Y",cit$year,sep="")
 # Updates Google spreadsheets, which updates the citations graph on my website!
